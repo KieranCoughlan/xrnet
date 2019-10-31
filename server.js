f50bd655a9f451c4fa518dd8b1f7b61b12d254a3
COUGHLAN <Kieran.Coughlan@woodgroup.com>
2019-10-31 14:11:45 +0000

    3

--- server.js
+++ server.js
@@ -9,5 +9,21 @@ console.log("Port number: " + port);
 
 const fs = require('fs');
 const WebSocket = require('ws');
+const path = require('path');
+const Channels = require('./channels');
+const Client = require('./client');
+const sessionDetails = getSessionDetails();
+
+const MAX_CLIENTS = 127;
+const SIZE_INT8 = 1;
+
+let clients = [];
+let rebroadcasts = 0;
+
+function getSessionDetails() {
+  let filePath = path.resolve(__dirname, 'session.json');
+  let asc = fs.readFileSync(filePath, 'utf8');
+  return JSON.parse(asc);
+}
 
 console.log("End");
